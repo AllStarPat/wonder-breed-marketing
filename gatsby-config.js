950cdd72095bdf822262e90b234c5263ec084c83
Patrick Shea <pat@Patricks-MacBook-Air-2.local>
2022-08-06 22:34:06 -0700

    updated the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `WonderBreed`,
+    siteTitleAlt: `WonderBreed - NFT breeding made simple`,
+    siteHeadline: `WonderBreed - NFT breeding made simple`,
+    siteUrl: `https://wonderbreed.com`,
+    siteDescription: `NFT breeding made simple`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@MetavrsAllStars`,
   },
   plugins: [
     {
